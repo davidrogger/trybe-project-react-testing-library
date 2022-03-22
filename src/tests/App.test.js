d7d744fd8519b20a04654a90548f5d2beba7253a
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-22 15:08:00 -0300

    iniciando testes do App - com primeiro teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import App from '../App';
+
+import renderWithRouter from './renderWithRouter';
+
+describe('Testing the component "<App />"', () => {
+  it('Should have 3 links with the text, "Home", "About" and "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+});
