2c3ec8b3a0675ced1b7b6317658df39cdfddfcdd
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-22 15:08:41 -0300

    segundo teste app

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from '../App';
 
@@ -16,5 +17,12 @@ describe('Testing the component "<App />"', () => {
     expect(homeLink).toBeInTheDocument();
     expect(aboutLink).toBeInTheDocument();
     expect(favoriteLink).toBeInTheDocument();
+  });
+  it('Should redirect the path to "/about" after clicking at "about" link', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
   });
 });
