f5364ad88f3f02ddb8887678ac244be4951384ab
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-22 16:04:39 -0300

    Requisito 4 - primeiro teste

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+
+import NotFound from '../components/NotFound';
+
+describe('Testing the component "<NotFound />"', () => {
+  it('Should have a heading text "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const headTitle = screen
+      .getByRole('heading', { level: 2, name: /page requested not found/i });
+    expect(headTitle).toBeInTheDocument();
+  });
+});
