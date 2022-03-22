407cbcdc8f540d1132ff26140ad555a4557bd318
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-22 15:50:31 -0300

    Requisito 3 - primeiro teste

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testing the component "<FavoritePokemons />"', () => {
+  it('Should have a message "No favorite pokemon found" when favorite is empty', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavorite = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
