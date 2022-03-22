1e8a76dcbed2b55e2f461224d0769d4e407e4aa6
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-22 15:33:00 -0300

    Requisito 2 - inicio com primeiro teste

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+
+import renderWithRouter from './renderWithRouter';
+
+describe('Testing the component "<About />"', () => {
+  it('Should a heading text with the text "About Pokédex"', () => {
+    renderWithRouter(<About />);
+    const headTitle = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(headTitle).toBeInTheDocument();
+  });
+});
