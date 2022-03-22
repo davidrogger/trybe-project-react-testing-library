9253d076bd08fe873bd3ca28061b3e5689c3196f
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-22 16:43:44 -0300

    Requisito 5 - primeiro teste

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import App from '../App';
+
+import renderWithRouter from './renderWithRouter';
+
+describe('Testing the component "<Pokedex />"', () => {
+  it('Should have a heading text "Encountered pokémons"', () => {
+    renderWithRouter(<App />);
+    const headTitle = screen.getByRole('heading', { name: 'Encountered pokémons' });
+    expect(headTitle).toBeInTheDocument();
+  });
+});
