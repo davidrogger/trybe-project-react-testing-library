0c00240cd711994164a9bb29a11585f9f12e9c47
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-23 13:06:36 -0300

    Requisito 6 - teste de rota e favoritos

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 
 import App from '../App';
 // import Pokemon from '../components/Pokemon';
@@ -28,7 +28,37 @@ describe('Testing component <Pokemon />', () => {
   });
   it('Should have a details link to gather more information about the pokemon', () => {
     renderWithRouter(<App />);
+    const pokemonId = pokemons[0].id;
+    const pokemonLink = `/pokemons/${pokemonId}`;
     const detailLink = screen.getByRole('link', { name: /more details/i });
     expect(detailLink).toBeInTheDocument();
+    expect(detailLink).toHaveAttribute('href', pokemonLink);
+  });
+  it('Should redirect to a new component with more details about de pokemon'
+  + ' after pressing details', () => {
+    const { history } = renderWithRouter(<App />);
+    const pokemonId = pokemons[0].id;
+    const detailLink = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(detailLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe(`/pokemons/${pokemonId}`);
+  });
+  it('Should render a star in the card when the pokemon is favorite'
+  + ' and not when isn\'t', () => {
+    renderWithRouter(<App />);
+    const detailLink = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(detailLink);
+    const favoriteCheckBox = screen.getByLabelText(/pokémon favoritado/i);
+    expect(favoriteCheckBox).toBeInTheDocument();
+    expect(favoriteCheckBox).toHaveProperty('checked', false);
+    const favoriteIcoDisable = screen.queryByRole('img', { name: /marked as favorite/i });
+    expect(favoriteIcoDisable).not.toBeInTheDocument();
+    userEvent.click(favoriteCheckBox);
+    expect(favoriteCheckBox).toHaveProperty('checked', true);
+    const favoriteIco = screen.getByRole('img', { name: /marked as favorite/i });
+    expect(favoriteIco).toHaveAttribute('src', '/star-icon.svg');
+    expect(favoriteIco).toBeInTheDocument();
+    const pokemonName = screen.getByTestId('pokemon-name').textContent;
+    expect(favoriteIco).toHaveAttribute('alt', `${pokemonName} is marked as favorite`);
   });
 });
